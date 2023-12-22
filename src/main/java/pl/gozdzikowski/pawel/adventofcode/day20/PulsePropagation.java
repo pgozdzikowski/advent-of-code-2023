e0cb678ac75f3f4b1682a1a1fa19c583519f6557
Paweł Goździkowski <pgozdzikowski@gmail.com>
2023-12-22 09:01:22 +0100

    Day 20 not working

---
+++ src/main/java/pl/gozdzikowski/pawel/adventofcode/day20/PulsePropagation.java
@@ -0,0 +1,212 @@
+package pl.gozdzikowski.pawel.adventofcode.day20;
+
+import pl.gozdzikowski.pawel.adventofcode.shared.collections.Pair;
+import pl.gozdzikowski.pawel.adventofcode.shared.input.Input;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.SequencedMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class PulsePropagation {
+
+    public Long calculateMultiplyOfLowAndHighPulse(Input input) {
+        String[] lines = input.getContent().split("\\n");
+        Map<String, ElfModule> elfModules = convertToModuleMap(lines);
+
+        Map<ElfModule, PulseSate> outputState = new HashMap<>(Map.of(elfModules.get("broadcaster"), PulseSate.LOW));
+        Pair<Long, Long> result = LongStream.range(0, 1000)
+                .mapToObj((ignore) -> propagateButtonForSinglePress(elfModules, outputState))
+                .reduce(Pair.of(0L, 0L), (acc, el) -> Pair.of(acc.left() + el.left(), acc.right() + el.right()));
+
+        return result.left() * result.right();
+    }
+
+    private Pair<Long, Long> propagateButtonForSinglePress(Map<String, ElfModule> elfModules, Map<ElfModule, PulseSate> outputState) {
+        Deque<List<ElfModule>> elfModulesToProcess = new LinkedList<>(List.of(List.of(elfModules.get("broadcaster"))));
+
+        Map<ElfModule, ElfModule> previousElfModule = new HashMap<>();
+        long lowSignals = 1;
+        long highSignals = 0;
+        while (!elfModulesToProcess.isEmpty()) {
+            List<ElfModule> elfModuleToProcess = elfModulesToProcess.poll();
+
+            SequencedMap<ElfModule, PulseSate> singleProcessedOutput = new LinkedHashMap<>();
+            for (ElfModule elfModule : elfModuleToProcess) {
+                SequencedMap<ElfModule, PulseSate> processed = elfModule.process(previousElfModule.get(elfModule), outputState.get(elfModule));
+                processed.sequencedKeySet().forEach((el) -> previousElfModule.put(el, elfModule));
+                singleProcessedOutput.putAll(processed);
+                elfModulesToProcess.add(new LinkedList<>(processed.sequencedKeySet()));
+            }
+            Map<PulseSate, List<Map.Entry<ElfModule, PulseSate>>> grouped = singleProcessedOutput.entrySet()
+                    .stream().collect(groupingBy(Map.Entry::getValue));
+            outputState.putAll(singleProcessedOutput);
+            lowSignals += grouped.getOrDefault(PulseSate.LOW, Collections.emptyList()).size();
+            highSignals += grouped.getOrDefault(PulseSate.HIGH, Collections.emptyList()).size();
+        }
+
+        return Pair.of(lowSignals, highSignals);
+    }
+
+    public Map<String, ElfModule> convertToModuleMap(String[] lines) {
+        Map<String, ElfModule> modules = new HashMap<>();
+        for (String line : lines) {
+            String[] splited = line.split("\\s->\\s");
+            ElfModule elfModule = fromNameFactory(splited[0]);
+            modules.put(elfModule.getName(), elfModule);
+        }
+
+        //make connections
+        for (String line : lines) {
+            String[] splited = line.split("\\s->\\s");
+            String[] outgoingModules = splited[1].split(",");
+            Stream.of(outgoingModules).filter((el) -> !modules.containsKey(el.trim())).forEach((el) -> modules.put(el.trim(), new ElfModule.NoOpModule(el.trim())));
+            Stream.of(outgoingModules).forEach((el) -> modules.get(convertName(splited[0])).addOutput(modules.get(el.trim())));
+            Stream.of(outgoingModules).forEach((el) -> modules.get(el.trim()).addInput(modules.get(convertName(splited[0]))));
+        }
+
+        return modules;
+    }
+
+    public String convertName(String name) {
+        if (Stream.of("%", "&").noneMatch(name::contains))
+            return name;
+        else {
+            return name.substring(1);
+        }
+    }
+
+    public ElfModule fromNameFactory(String nameWithType) {
+        if (Stream.of("%", "&").noneMatch(nameWithType::contains)) {
+            return new ElfModule.NoOpModule(nameWithType);
+        }
+
+        return switch (nameWithType.charAt(0)) {
+            case '%' -> new ElfModule.FlipFlowModule(nameWithType.substring(1));
+            case '&' -> new ElfModule.ConjunctionModule(nameWithType.substring(1));
+            default -> throw new IllegalStateException("Unable to find valid module");
+        };
+    }
+
+    public static sealed abstract class ElfModule permits ElfModule.ConjunctionModule, ElfModule.FlipFlowModule, ElfModule.NoOpModule {
+
+        protected List<ElfModule> outputs;
+        protected List<ElfModule> inputs;
+        private String name;
+
+
+        public abstract SequencedMap<ElfModule, PulseSate> process(ElfModule sourceModule, PulseSate pulseSate);
+
+        public ElfModule(String name) {
+            this.outputs = new ArrayList<>();
+            this.inputs = new ArrayList<>();
+            this.name = name;
+        }
+
+        public void addInput(ElfModule elfModule) {
+            this.inputs.add(elfModule);
+        }
+
+        public void addOutput(ElfModule elfModule) {
+            this.outputs.add(elfModule);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ElfModule elfModule = (ElfModule) o;
+            return Objects.equals(name, elfModule.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        protected SequencedMap<ElfModule, PulseSate> convertToOutputPulseMap(PulseSate pulseSate) {
+            return outputs.stream().collect(Collectors.toMap(Function.identity(), (el -> pulseSate), (u, v) -> {
+                throw new IllegalStateException(String.format("Duplicate key %s", u));
+            }, LinkedHashMap::new));
+        }
+
+        public static final class FlipFlowModule extends ElfModule {
+            public boolean tunedOn = false;
+
+            public FlipFlowModule(String name) {
+                super(name);
+            }
+
+            @Override
+            public SequencedMap<ElfModule, PulseSate> process(ElfModule sourceModule, PulseSate pulseSate) {
+                if (pulseSate == PulseSate.HIGH) {
+                    return new LinkedHashMap<>();
+                }
+
+                tunedOn = !tunedOn;
+                PulseSate pulseSateAfterPass = !tunedOn ? PulseSate.LOW : PulseSate.HIGH;
+
+                return convertToOutputPulseMap(pulseSateAfterPass);
+            }
+        }
+
+        public static final class ConjunctionModule extends ElfModule {
+
+            Map<ElfModule, PulseSate> memory = new HashMap<>();
+
+            public ConjunctionModule(String name) {
+                super(name);
+            }
+
+            @Override
+            public SequencedMap<ElfModule, PulseSate> process(ElfModule sourceModule, PulseSate pulseSate) {
+                if (sourceModule != null)
+                    memory.put(sourceModule, pulseSate);
+                PulseSate outputPulse = memory.values().stream().allMatch((el) -> el == PulseSate.HIGH) ? PulseSate.LOW : PulseSate.HIGH;
+                return convertToOutputPulseMap(outputPulse);
+            }
+
+            @Override
+            public void addInput(ElfModule elfModule) {
+                super.addInput(elfModule);
+                memory.put(elfModule, PulseSate.LOW);
+            }
+
+        }
+
+        public static final class NoOpModule extends ElfModule {
+
+            @Override
+            public SequencedMap<ElfModule, PulseSate> process(ElfModule sourceModule, PulseSate pulseSate) {
+
+                return convertToOutputPulseMap(pulseSate);
+            }
+
+            public NoOpModule(String name) {
+                super(name);
+            }
+        }
+    }
+
+
+    public enum PulseSate {
+        LOW, HIGH
+    }
+
+}
---
+++ src/test/groovy/pl/gozdzikowski/pawel/adventofcode/day20/Day20Spec.groovy
@@ -0,0 +1,48 @@
+package pl.gozdzikowski.pawel.adventofcode.day20
+
+import pl.gozdzikowski.pawel.adventofcode.shared.input.FileInput
+import pl.gozdzikowski.pawel.adventofcode.shared.input.Input
+import pl.gozdzikowski.pawel.adventofcode.shared.input.StringInput
+import spock.lang.Specification
+
+class Day20Spec extends Specification{
+    PulsePropagation pulsePropagation = new PulsePropagation()
+
+    def 'should propagate pulse correctly'() {
+        given:
+            def input = """broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+"""
+        when:
+            def result = pulsePropagation.calculateMultiplyOfLowAndHighPulse(new StringInput(input))
+        then:
+            result == 32000000
+    }
+
+    def 'sample 2'() {
+        given:
+            def input = """broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+"""
+        when:
+            def result = pulsePropagation.calculateMultiplyOfLowAndHighPulse(new StringInput(input))
+        then:
+            result == 11687500
+    }
+
+    def 'part1'() {
+        given:
+            Input input = new FileInput('day20.txt')
+        when:
+            def pulse = pulsePropagation.calculateMultiplyOfLowAndHighPulse(input)
+        then:
+            pulse == 2004545842
+    }
+
+}
